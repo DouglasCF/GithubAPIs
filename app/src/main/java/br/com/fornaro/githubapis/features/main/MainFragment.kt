0872b42144e3ddb54561c5401c136a1218ea55a5
Douglas Fornaro <douglasfornaro@hotmail.com>
2021-03-22 19:53:35 -0300

    Configuring the dependencies and main screen
    - Adding the navigation and some others dependencies.
    - Created the Main screen as empty for now.

---
+++ app/src/main/java/br/com/fornaro/githubapis/features/main/MainFragment.kt
@@ -0,0 +1,19 @@
+package br.com.fornaro.githubapis.features.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import br.com.fornaro.githubapis.R
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+}
---
+++ app/src/main/java/br/com/fornaro/githubapis/features/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package br.com.fornaro.githubapis.features.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
