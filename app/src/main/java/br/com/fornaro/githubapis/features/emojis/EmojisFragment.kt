5718dbebb8154bead141554b3dc78dd905f6d350
Douglas Fornaro <douglasfornaro@hotmail.com>
2021-03-27 11:43:28 -0300

    Handle error and loading

--- app/src/main/java/br/com/fornaro/githubapis/features/emojis/EmojisFragment.kt
+++ app/src/main/java/br/com/fornaro/githubapis/features/emojis/EmojisFragment.kt
@@ -2,35 +2,29 @@ package br.com.fornaro.githubapis.features.emojis
 
 import android.os.Bundle
 import android.view.View
-import androidx.fragment.app.Fragment
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import br.com.fornaro.githubapis.R
 import br.com.fornaro.githubapis.databinding.FragmentEmojisBinding
+import br.com.fornaro.githubapis.features.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class EmojisFragment : Fragment(R.layout.fragment_emojis) {
+class EmojisFragment : BaseFragment<FragmentEmojisBinding>(R.layout.fragment_emojis) {
 
     private val viewModel: EmojisViewModel by viewModels()
 
     private val viewAdapter by lazy(LazyThreadSafetyMode.NONE) { EmojiAdapter() }
 
-    private var _binding: FragmentEmojisBinding? = null
-    private val binding get() = _binding!!
+    override fun bind(view: View) = FragmentEmojisBinding.bind(view)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        _binding = FragmentEmojisBinding.bind(view)
         setupRecyclerView()
         setupViewModel()
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 
     private fun setupRecyclerView() = with(binding.rvEmojis) {
@@ -40,16 +34,22 @@ class EmojisFragment : Fragment(R.layout.fragment_emojis) {
 
     private fun setupViewModel() = with(viewModel) {
         lifecycleScope.launch { state.collect(::handleState) }
+        lifecycleScope.launch { message.collect(::handleMessage) }
     }
 
-    private fun handleState(state: EmojisState) = when (state) {
-        is EmojisState.Content -> handleContent(state)
-        EmojisState.Loading -> {
-
+    private fun handleState(state: EmojisState) {
+        handleLoading(false)
+        when (state) {
+            is EmojisState.Content -> handleContent(state)
+            EmojisState.Loading -> handleLoading(true)
         }
     }
 
     private fun handleContent(content: EmojisState.Content) {
         viewAdapter.list = content.emojis
+    }
+
+    private fun handleLoading(loading: Boolean) {
+        binding.pbEmojis.isVisible = loading
     }
 }
--- app/src/main/java/br/com/fornaro/githubapis/features/main/MainFragment.kt
+++ app/src/main/java/br/com/fornaro/githubapis/features/main/MainFragment.kt
@@ -2,36 +2,29 @@ package br.com.fornaro.githubapis.features.main
 
 import android.os.Bundle
 import android.view.View
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
 import br.com.fornaro.githubapis.R
 import br.com.fornaro.githubapis.databinding.FragmentMainBinding
+import br.com.fornaro.githubapis.features.BaseFragment
 import coil.load
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class MainFragment : Fragment(R.layout.fragment_main) {
+class MainFragment : BaseFragment<FragmentMainBinding>(R.layout.fragment_main) {
 
     private val viewModel: MainViewModel by viewModels()
 
-    private var _binding: FragmentMainBinding? = null
-    private val binding get() = _binding!!
+    override fun bind(view: View) = FragmentMainBinding.bind(view)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        _binding = FragmentMainBinding.bind(view)
         setupRandomEmojiButton()
         setupEmojiListButton()
         setupViewModel()
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 
     private fun setupRandomEmojiButton() = with(binding.bMainRandomEmoji) {
@@ -44,10 +37,10 @@ class MainFragment : Fragment(R.layout.fragment_main) {
 
     private fun setupViewModel() = with(viewModel) {
         lifecycleScope.launch { state.collect(::handleState) }
+        lifecycleScope.launch { message.collect(::handleMessage) }
     }
 
     private fun handleState(state: MainState) {
         binding.imMain.load(state.emoji?.url)
     }
-
 }
